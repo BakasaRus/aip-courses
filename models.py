d1607b2bfacadc18f22abb0e002df9c7d9402b13
Nikolay Andreev <bakasaru@list.ru>
2022-04-06 18:47:44 +0300

    Fix password hashing

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class User(db.Model, UserMixin):
     courses = db.relationship('Course', backref='owner')
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
